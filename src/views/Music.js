8cfa3fff7a9b35229e83628e7c088d5710e334ce
Dionne <mylife33@hotmail.com>
2025-09-11 18:42:05 -0500

    added search

--- src/views/Music.js
+++ src/views/Music.js
@@ -18,13 +18,48 @@ export default function SongList({ setCurrentSongId, setSongs }) {
 
   return (
     <>
-      <div className="perspective-dropdown">
-        <select onChange={(e) => changePerspective(e.target.value)}>
-          <option value="all">All Songs</option>
-          <option value="to Jesus">To Jesus</option>
-          <option value="from Jesus">From Jesus</option>
-          <option value="about Jesus">About Jesus</option>
-        </select>
+      <div
+        className="filters"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "20px",
+        }}
+      >
+        <div className="search-bar" style={{ justifyContent: "center" }}>
+          <input
+            style={{
+              padding: "5px",
+              fontSize: "16px",
+              backgroundColor: "#e7e5e5",
+            }}
+            type="text"
+            placeholder="Search songs..."
+            onChange={(e) => {
+              const query = e.target.value.toLowerCase();
+              const filtered = reversedList.filter((song) =>
+                song.title.toLowerCase().includes(query)
+              );
+              setFilteredSongs(filtered);
+            }}
+          />
+        </div>
+        <div style={{ marginLeft: "20px" }}>
+          <select
+            onChange={(e) => changePerspective(e.target.value)}
+            style={{
+              padding: "5px",
+              fontSize: "16px",
+              backgroundColor: "#e7e5e5",
+            }}
+          >
+            <option value="all">All Songs</option>
+            <option value="to Jesus">To Jesus</option>
+            <option value="from Jesus">From Jesus</option>
+            <option value="about Jesus">About Jesus</option>
+          </select>
+        </div>
       </div>
       <div className="song-list">
         {filteredSongs?.map((song) => (
