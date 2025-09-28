588b50f01a01070519d44eea7c211cb260532ff5
Dionne <mylife33@hotmail.com>
2025-09-27 21:47:26 -0500

    hot fix remove warnings

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ export default function App() {
       .catch((error) => {
         console.error("Error fetching songs:", error);
       });
-  }, []);
+  });
 
   // Centralized filtering (UI calls this)
   const filterSongs = (type, value) => {
